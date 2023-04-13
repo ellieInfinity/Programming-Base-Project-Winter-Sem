073c150c1a919d4253e49f577308b88591d48c01
EuanV2004 <euan.volkmann@ontariotechu.net>
2023-04-13 19:01:01 -0400

    Added generics

---
+++ Metroidvania Base Project Tutorial/Assets/Scripts/Lab 10/Generics.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generics : MonoBehaviour
+{
+    private int[] intArr = { 1, 2, 3 };
+    private float[] floatArr = { 1.1f, 2.2f, 3.3f };
+    private string[] strArr = { "Aa", "Bb", "Cc" };
+
+    private void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            DisplayElements(intArr);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            DisplayElements(floatArr);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            DisplayElements(strArr);
+        }
+    }
+
+    // We can use generics to have a single method which can accept any data type
+    public void DisplayElements<Thingy>(Thingy[] arr) 
+    {
+        foreach (Thingy thing in arr) {
+            print(thing);
+        }
+    }
+}
